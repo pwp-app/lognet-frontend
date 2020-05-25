db0f2d4dd9a28da35098d3d95988071efb88096c
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-26 02:56:52 +0800

    add: recaptcha

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { ConfigProvider } from 'antd';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import keys from './config/keys';
 import './styles/main.less';
 
 ReactDOM.render(
     <ConfigProvider autoInsertSpaceInButton={false}>
-        <App />
+        <GoogleReCaptchaProvider reCaptchaKey={keys.recaptcha} language="zh-CN" useRecaptchaNet="true">
+            <App />
+        </GoogleReCaptchaProvider>
     </ConfigProvider>,
     document.getElementById('root')
 );
