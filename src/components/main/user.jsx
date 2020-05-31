5fda67ca6725aa01c08ea736d5ff14229dad54c0
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-06-01 03:04:32 +0800

    add: user drop down events

--- src/components/main/user.jsx
+++ src/components/main/user.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
-import { Menu, Dropdown, Avatar } from 'antd';
+import { Menu, Dropdown, Avatar, message } from 'antd';
 import { UserOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import axios from '../../utils/axios';
 import MD5 from 'crypto-js/md5';
-
-const menu = (
-    <Menu>
-        <Menu.Item>
-            <SettingOutlined />
-            <span>用户设置</span>
-        </Menu.Item>
-        <Menu.Item>
-            <LogoutOutlined />
-            <span>退出登录</span>
-        </Menu.Item>
-    </Menu>
-);
 
 const mapState = state => ({
     user: state.user,
 });
 
 class UserDropdown extends React.Component {
+    toUserSettings = () => {
+        this.props.history.push('/app/user');
+    }
+    logout = () => {
+        axios.get('/portal/logout').then((res) => {
+            if (res.data && res.data.code === 200) {
+                message.success('登出成功');
+                this.props.history.push('');
+            } else {
+                message.error('登出失败，' + res.data.message);
+            }
+        }).catch(() => {
+            message.error('登出失败');
+        });
+    }
+    menu = (
+        <Menu>
+            <Menu.Item onClick={this.toUserSettings}>
+                <SettingOutlined />
+                <span>用户设置</span>
+            </Menu.Item>
+            <Menu.Item onClick={this.logout}>
+                <LogoutOutlined />
+                <span>退出登录</span>
+            </Menu.Item>
+        </Menu>
+    );
     render() {
         const email_hash = this.props.user.email ? MD5(this.props.user.email) : null;
         return (
-            <Dropdown overlay={menu}>
+            <Dropdown overlay={this.menu}>
                 <div className="user-dropdown">
                     {
                         this.props.user.email ? <Avatar className="user-dropdown-avatar" src={`https://www.gravatar.com/avatar/${email_hash}?s=32`}/>: <Avatar className="user-dropdown-avatar" icon={<UserOutlined />} />
@@ -37,4 +53,4 @@ class UserDropdown extends React.Component {
     }
 }
 
-export default connect(mapState, null)(UserDropdown);
+export default connect(mapState, null)(withRouter(UserDropdown));
