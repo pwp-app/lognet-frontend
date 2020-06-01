a9146d4f225c6419d79aa82617a0388ce8623e6b
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-06-01 13:50:59 +0800

    add: click function

--- src/components/main/settingcard.jsx
+++ src/components/main/settingcard.jsx
@@ -3,13 +3,18 @@ import { Card } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 
 class SettingCard extends React.Component {
+    actionClicked = (e) => {
+        if (typeof this.props.action === 'function') {
+            this.props.action(e);
+        }
+    }
     render() {
         return (
             <Card className="setting-card" title={this.props.title}>
                 <div className="setting-card-content">
                     { this.props.content }
                 </div>
-                <div className="setting-card-action">
+                <div className="setting-card-action" onClick={this.actionClicked}>
                     <span>{ this.props.actionName }<RightOutlined/></span>
                 </div>
             </Card>
