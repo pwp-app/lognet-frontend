8e249f3a9e947ec85ceb7e22468ad110b11cce26
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-06-01 14:21:40 +0800

    add: action clicked

--- src/components/main/settingiconcard.jsx
+++ src/components/main/settingiconcard.jsx
@@ -4,6 +4,11 @@ import { RightOutlined } from '@ant-design/icons';
 import Icon from '../common/icon';
 
 class SettingIconCard extends React.Component {
+    actionClicked = (e) => {
+        if (typeof this.props.action === 'function') {
+            this.props.action(e);
+        }
+    };
     render() {
         return (
             <Card className="setting-iconcard">
@@ -14,15 +19,16 @@ class SettingIconCard extends React.Component {
                     <div className="setting-iconcard-title">
                         <span>{this.props.title}</span>
                     </div>
-                    <div className="setting-iconcard-content">
-                        {this.props.content}
-                    </div>
-                    <div className="setting-iconcard-action">
-                        <span>{this.props.actionName}<RightOutlined/></span>
+                    <div className="setting-iconcard-content">{this.props.content}</div>
+                    <div className="setting-iconcard-action" onClick={this.actionClicked}>
+                        <span>
+                            {this.props.actionName}
+                            <RightOutlined />
+                        </span>
                     </div>
                 </div>
             </Card>
-        )
+        );
     }
 }
 
