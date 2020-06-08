8d10547590ec85d285f1f075f44111f120f42eea
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-06-08 12:44:43 +0800

    fix: nav

--- src/components/main/nav.jsx
+++ src/components/main/nav.jsx
@@ -38,8 +38,12 @@ class Nav extends React.Component {
     }
     componentDidMount() {
         let path = window.location.pathname;
-        if (path.endsWith('/')) {
-            path = path.substring(0, path.length - 1);
+        if (path.includes('/app/sites')) {
+            path = '/app/sites';
+        } else {
+            if (path.endsWith('/')) {
+                path = path.substring(0, path.length - 1);
+            }
         }
         this.setState({
             activeKey: path,
